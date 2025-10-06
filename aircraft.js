904a25d11679ff2b2483fcefa46a50d50396227e
Sibineshwaran S <sibineshwarans@gmail.com>
2025-10-06 21:51:02 +0530

    Update aircraft.js

--- aircraft.js
+++ aircraft.js
@@ -1,7 +1,7 @@
 // routes/aircraft.js
 const express = require("express");
 const router = express.Router();
-const pool = require("db");
+const pool = require("./db");
 
 /**
  * A helper function to generate the seat map layout object.
@@ -179,3 +179,4 @@ router.delete("/delete/:id", async (req, res) => {
 
 
 module.exports = router;
+
